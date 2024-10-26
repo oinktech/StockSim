6f878e3e76fd6cf201c84598a6d0f0ccf5f7d861
oinktech <oinktech2024@gmail.com>
2024-10-26 20:25:24 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
+
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    MONGO_URI = os.getenv('MONGO_URI')
-    FINNHUB_API_KEY = os.getenv('FINNHUB_API_KEY')
-    GMAIL_USER = os.getenv('GMAIL_USER')
-    GMAIL_PASSWORD = os.getenv('GMAIL_PASSWORD')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    FINNHUB_API_KEY = os.environ.get('FINNHUB_API_KEY')
+    GMAIL_USER = os.environ.get('GMAIL_USER')
+    GMAIL_PASSWORD = os.environ.get('GMAIL_PASSWORD')
